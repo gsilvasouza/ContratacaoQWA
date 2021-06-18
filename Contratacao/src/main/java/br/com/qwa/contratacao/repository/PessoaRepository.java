b400355c4dcaa0ac8bbdd9f1235e57054b18c770
gsilvasouza <gsilvasouza2000@gmail.com>
2021-06-17 23:58:38 -0300

    Adição da anotação Repository

--- Contratacao/src/main/java/br/com/qwa/contratacao/repository/PessoaRepository.java
+++ Contratacao/src/main/java/br/com/qwa/contratacao/repository/PessoaRepository.java
@@ -2,6 +2,8 @@ package br.com.qwa.contratacao.repository;
 
 import br.com.qwa.contratacao.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
 }

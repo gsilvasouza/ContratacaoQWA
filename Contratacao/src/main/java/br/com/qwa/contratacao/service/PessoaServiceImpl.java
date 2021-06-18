b38868e95d3caeba8bc41cb11ddf9ea0c61f2846
gsilvasouza <gsilvasouza2000@gmail.com>
2021-06-17 23:58:21 -0300

    Adição da anotação Service

--- Contratacao/src/main/java/br/com/qwa/contratacao/service/PessoaServiceImpl.java
+++ Contratacao/src/main/java/br/com/qwa/contratacao/service/PessoaServiceImpl.java
@@ -2,7 +2,9 @@ package br.com.qwa.contratacao.service;
 
 import br.com.qwa.contratacao.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PessoaServiceImpl extends BaseServiceImpl<Pessoa, Integer> implements PessoaService {
     public PessoaServiceImpl(JpaRepository<Pessoa, Integer> repository) {
         super(repository);

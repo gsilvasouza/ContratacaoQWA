e94c128b565ec406f4670847865f5e385c107885
gsilvasouza <gsilvasouza2000@gmail.com>
2021-06-17 22:23:06 -0300

    Criação da controller PessoaController

---
+++ Contratacao/src/main/java/br/com/qwa/contratacao/controller/PessoaController.java
@@ -0,0 +1,16 @@
+package br.com.qwa.contratacao.controller;
+
+import br.com.qwa.contratacao.service.PessoaService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/pessoa")
+public class PessoaController {
+
+    private final PessoaService pessoaService;
+
+    public PessoaController(PessoaService pessoaService) {
+        this.pessoaService = pessoaService;
+    }
+}

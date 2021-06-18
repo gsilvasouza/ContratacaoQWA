a92c9cb12eb7c0b449d09a636fd53ba1fec0d282
gsilvasouza <gsilvasouza2000@gmail.com>
2021-06-17 22:22:37 -0300

    Adição da anotação @Service

--- Contratacao/src/main/java/br/com/qwa/contratacao/service/PessoaService.java
+++ Contratacao/src/main/java/br/com/qwa/contratacao/service/PessoaService.java
@@ -1,6 +1,8 @@
 package br.com.qwa.contratacao.service;
 
 import br.com.qwa.contratacao.model.Pessoa;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface PessoaService extends BaseService<Pessoa, Integer> {
 }
